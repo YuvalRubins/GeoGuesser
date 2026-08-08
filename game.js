f09eeafff09f420bed1ddcf9392030a7286688fb
Yuval Rubins <128133566+YuvalRubins@users.noreply.github.com>
2026-08-08 16:54:02 +0300

    Update locations

--- game.js
+++ game.js
@@ -5,28 +5,31 @@
 */
 
 
-const locations = [
+const locations = const locations = [
 
     {
-        name: "Statue of Liberty, New York",
-        lat: 40.6892,
-        lon: -74.0445
+        name: "Ben Gurion Airport, Israel",
+        lat: 32.0055,
+        lon: 34.8854
     },
-
 
     {
-        name: "Golden Gate Bridge, San Francisco",
-        lat: 37.8199,
-        lon: -122.4783
+        name: "Tortuguero, Costa Rica",
+        lat: 10.5427,
+        lon: -83.5026
     },
-
 
     {
-        name: "Chichén Itzá, Mexico",
-        lat: 20.6843,
-        lon: -88.5678
+        name: "Monteverde, Costa Rica",
+        lat: 10.3157,
+        lon: -84.8251
     },
 
+    {
+        name: "Jacó, Costa Rica",
+        lat: 9.6140,
+        lon: -84.5370
+    },
 
     {
         name: "Antigua Guatemala",
@@ -34,27 +37,11 @@ const locations = [
         lon: -90.7295
     },
 
-
     {
-        name: "Cancún, Mexico",
-        lat: 21.1619,
-        lon: -86.8515
+        name: "Lake Atitlán, Guatemala",
+        lat: 14.6907,
+        lon: -91.2025
     },
-
-
-    {
-        name: "Yellowstone National Park",
-        lat: 44.4280,
-        lon: -110.5885
-    },
-
-
-    {
-        name: "Disneyland, California",
-        lat: 33.8121,
-        lon: -117.9190
-    },
-
 
     {
         name: "Tikal, Guatemala",
@@ -62,11 +49,94 @@ const locations = [
         lon: -89.6237
     },
 
+    {
+        name: "Bacalar, Mexico",
+        lat: 18.6781,
+        lon: -88.3920
+    },
 
     {
-        name: "San José, Costa Rica",
-        lat: 9.9281,
-        lon: -84.0907
+        name: "Chichén Itzá, Mexico",
+        lat: 20.6843,
+        lon: -88.5678
+    },
+
+    {
+        name: "Holbox, Mexico",
+        lat: 21.5236,
+        lon: -87.3781
+    },
+
+    {
+        name: "Washington Monument, Washington DC",
+        lat: 38.8895,
+        lon: -77.0353
+    },
+
+    {
+        name: "Liberty Bell, Philadelphia",
+        lat: 39.9496,
+        lon: -75.1503
+    },
+
+    {
+        name: "Statue of Liberty, New York",
+        lat: 40.6892,
+        lon: -74.0445
+    },
+
+    {
+        name: "Nantucket, Massachusetts",
+        lat: 41.2835,
+        lon: -70.0995
+    },
+
+    {
+        name: "Niagara Falls",
+        lat: 43.0962,
+        lon: -79.0377
+    },
+
+    {
+        name: "Golden Gate Bridge, San Francisco",
+        lat: 37.8199,
+        lon: -122.4783
+    },
+
+    {
+        name: "Disneyland, California",
+        lat: 33.8121,
+        lon: -117.9190
+    },
+
+    {
+        name: "The Strip, Las Vegas",
+        lat: 36.1147,
+        lon: -115.1728
+    },
+
+    {
+        name: "Grand Canyon, Arizona",
+        lat: 36.0544,
+        lon: -112.1401
+    },
+
+    {
+        name: "Salt Lake City, Utah",
+        lat: 40.7608,
+        lon: -111.8910
+    },
+
+    {
+        name: "Yellowstone National Park",
+        lat: 44.4280,
+        lon: -110.5885
+    },
+
+    {
+        name: "Space Needle, Seattle",
+        lat: 47.6205,
+        lon: -122.3493
     }
 
 ];
